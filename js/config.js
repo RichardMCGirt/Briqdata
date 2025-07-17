23caa8da865c61c53cffbbbde296f5af968d35c8
unknown <richard@vanirinstalledsales.com>
2025-07-17 14:48:26 -0400

    loading bar

---
+++ js/config.js
@@ -0,0 +1,7 @@
+window.GOOGLE_CONFIG = {
+  CLIENT_ID: "518347118969-drq9o3vr7auf78l16qcteor9ng4nv7qd.apps.googleusercontent.com",
+  API_KEY: "AIzaSyBGYsHkTEvE9eSYo9mFCUIecMcQtT8f0hg",
+  SHEET_ID: "1-odgue-k8jX-QbnTvi-7_jUK4O2Q---ZnMzuuBfKYdE",
+  DISCOVERY_DOCS: ["https://sheets.googleapis.com/$discovery/rest?version=v4"],
+  SCOPES: "https://www.googleapis.com/auth/spreadsheets.readonly"
+};
